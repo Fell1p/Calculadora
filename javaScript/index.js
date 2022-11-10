8b83d236b9d20919d193f387064fa70cef68209a
Fell1p <luofelipe@hotmail.com>
2022-11-09 23:18:36 -0300

    Solução de bug no visor, estilização das teclas de operações e o reset ao clicar na tecla de igual

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,15 +1,15 @@
-let display = document.querySelector("#display")
+let display = document.querySelector(".visor")
 let res = document.querySelector(".result")
 let limpa = document.querySelector(".tecla-limpar")
 
 function calcular(){
-    
+
     let resultado = eval(display.value)   
     res.innerHTML = `${resultado}`
 
-    console.log(resultado)
+    display.value = " "
 }
 
-function limpar(limpa){
+function limpar(){
     res.innerHTML = " "
 }
